e020fe4e2780ae896139d34e176521f0faea2f61
Pavel Rogozhin <rogozhinp@gmail.com>
2021-09-28 18:40:03 +0300

    add junit5

---
+++ src/test/java/edu/princeton/cs/src/simplequeue/SimpleQueueTest.java
@@ -0,0 +1,21 @@
+package edu.princeton.cs.src.simplequeue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author hadson
+ */
+public class SimpleQueueTest
+{
+    
+    public SimpleQueueTest()
+    {
+    }
+
+    @Test
+    public void testReadInts() {
+        System.out.println("test is ok");
+    }
+    
+}
