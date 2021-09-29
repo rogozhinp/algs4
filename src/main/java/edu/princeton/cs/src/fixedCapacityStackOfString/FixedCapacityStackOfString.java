25eb184314b3ebc56fca8c5e3f1f02049c162bfc
Pavel Rogozhin <rogozhinp@gmail.com>
2021-09-29 12:43:24 +0300

    FixedCapacityStackOfStrings done

---
+++ src/main/java/edu/princeton/cs/src/fixedCapacityStackOfString/FixedCapacityStackOfString.java
@@ -0,0 +1,9 @@
+package edu.princeton.cs.src.fixedCapacityStackOfString;
+
+public interface FixedCapacityStackOfString {
+
+    void push(String item);
+    String pop();
+    boolean isEmpty();
+    int size();
+}
---
+++ src/main/java/edu/princeton/cs/src/fixedCapacityStackOfString/FixedCapacityStackOfStringImpl.java
@@ -0,0 +1,36 @@
+package edu.princeton.cs.src.fixedCapacityStackOfString;
+
+public class FixedCapacityStackOfStringImpl implements FixedCapacityStackOfString{
+    
+    private String[] a;
+    private int N;
+
+    public FixedCapacityStackOfStringImpl(int cap) {
+        a = new String[cap];
+    }
+    
+    
+          
+
+    @Override
+    public void push(String item) {
+        a[N++] = item;
+    }
+
+    @Override
+    public String pop() {
+        return a[--N];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    @Override
+    public int size() {
+        return N;
+    }
+
+
+}
